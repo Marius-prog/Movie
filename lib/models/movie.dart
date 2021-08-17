e98781e0958d659e78d453e2e343c36625fcc845
Marius Sabaliauskas <rha2r4qef9@privaterelay.appleid.com>
2021-08-17 07:27:35 +0200

    api data modeling

---
+++ lib/models/movie.dart
@@ -0,0 +1,45 @@
+import 'package:get_it/get_it.dart';
+
+import 'package:movie_tmdb/models/app_config.dart';
+
+import '../models/app_config.dart';
+
+class Movie {
+  final String name;
+  final String language;
+  final bool isAdult;
+  final String description;
+  final String posterPath;
+  final String backdropPath;
+  final num rating;
+  final String releaseDate;
+
+  Movie({
+    required this.name,
+    required this.language,
+    required this.isAdult,
+    required this.description,
+    required this.posterPath,
+    required this.backdropPath,
+    required this.rating,
+    required this.releaseDate,
+  });
+
+  factory Movie.fromJson(Map<String, dynamic> _json) {
+    return Movie(
+      name: _json['title'],
+      language: _json['original_language'],
+      isAdult: _json['adult'],
+      description: _json['overview'],
+      posterPath: _json['poster_path'],
+      backdropPath: _json['backdrop_path'],
+      rating: _json['vote_averge'],
+      releaseDate: _json['release_date'],
+    );
+  }
+
+  String posterURL() {
+    final AppConfig _appConfig = GetIt.instance.get<AppConfig>();
+    return '${_appConfig.BASE_IMAGE_API_URL}${this.posterPath}';
+  }
+}
---
+++ lib/pages/main_page.dart
@@ -0,0 +1,210 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+//widget
+import '../widgets/movie_tile.dart';
+
+import '../models/search_category.dart';
+import '../models/movie.dart';
+
+class MainPage extends ConsumerWidget {
+  late double _deviceHeight;
+  late double _deviceWidth;
+
+  late TextEditingController _searchTextFieldController;
+
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+
+    _searchTextFieldController = TextEditingController();
+    return _buildUI();
+  }
+
+  Widget _buildUI() {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.black,
+      body: Container(
+        height: _deviceHeight,
+        width: _deviceWidth,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            _backgroundWidget(),
+            _foregroundWidgets(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _backgroundWidget() {
+    return Container(
+      height: _deviceHeight,
+      width: _deviceWidth,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        image: DecorationImage(
+          image: NetworkImage(
+              'https://img.artpal.com/179971/46-20-7-7-19-20-8m.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 15.0, sigmaY: 15.0),
+        child: Container(
+          color: Colors.blue.withOpacity(0.001),
+        ),
+      ),
+    );
+  }
+
+  Widget _foregroundWidgets() {
+    return Container(
+      padding: EdgeInsets.fromLTRB(0, _deviceHeight * 0.02, 0, 0),
+      width: _deviceWidth * 0.88,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _topBarWidget(),
+          Container(
+            height: _deviceHeight * 0.83,
+            padding: EdgeInsets.symmetric(vertical: _deviceHeight * 0.01),
+            child: _moviesListViewWidget(),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _topBarWidget() {
+    return Container(
+      height: _deviceHeight * 0.08,
+      decoration: BoxDecoration(
+        color: Colors.black45,
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _searchFieldWidget(),
+          _categorySelectionWidget(),
+        ],
+      ),
+    );
+  }
+
+  Widget _searchFieldWidget() {
+    final _border = InputBorder.none;
+
+    return Container(
+      width: _deviceWidth * 0.58,
+      height: _deviceHeight * 0.05,
+      child: TextField(
+        controller: _searchTextFieldController,
+        onSubmitted: (_input) {},
+        style: TextStyle(color: Colors.white),
+        decoration: InputDecoration(
+            focusedBorder: _border,
+            border: _border,
+            prefixIcon: Icon(Icons.search, color: Colors.white24),
+            hintStyle: TextStyle(color: Colors.white54),
+            filled: false,
+            fillColor: Colors.white24,
+            hintText: 'Search..'),
+      ),
+    );
+  }
+
+  Widget _categorySelectionWidget() {
+    return DropdownButton(
+      dropdownColor: Colors.black38,
+      value: SearchCategory.popular,
+      icon: Icon(
+        Icons.menu,
+        color: Colors.white24,
+      ),
+      underline: Container(
+        height: 1,
+        color: Colors.white24,
+      ),
+      onChanged: (_value) {},
+      items: [
+        DropdownMenuItem(
+          child: Text(
+            SearchCategory.popular,
+            style: TextStyle(color: Colors.white),
+          ),
+          value: SearchCategory.popular,
+        ),
+        DropdownMenuItem(
+          child: Text(
+            SearchCategory.upcoming,
+            style: TextStyle(color: Colors.white),
+          ),
+          value: SearchCategory.upcoming,
+        ),
+        DropdownMenuItem(
+          child: Text(
+            SearchCategory.none,
+            style: TextStyle(color: Colors.white),
+          ),
+          value: SearchCategory.none,
+        ),
+      ],
+    );
+  }
+
+  Widget _moviesListViewWidget() {
+    final List<Movie> _movies = [];
+
+    for (var i = 0; i < 20; i++) {
+      _movies.add(
+        Movie(
+          name: "Mortal Kombat",
+          language: "EN",
+          isAdult: false,
+          description:
+              "MMA fighter Cole Young seeks out Earth's greatest champions in order to stand against the enemies of Outworld in a high stakes battle for the universe.",
+          posterPath: "/kb4s0ML0iVZlG6wAKbbs9NAm6X.jpg",
+          backdropPath: "/rAgsOIhqRS6tUthmHoqnqh9PIAE.jpg",
+          rating: 7.8,
+          releaseDate: "2021-08-12",
+        ),
+      );
+    }
+
+    if (_movies.length != 0) {
+      return ListView.builder(
+          itemCount: _movies.length,
+          itemBuilder: (BuildContext _context, int _count) {
+            return Padding(
+              padding: EdgeInsets.symmetric(
+                  vertical: _deviceHeight * 0.01, horizontal: 0),
+              child: GestureDetector(
+                onTap: () {},
+                child: MovieTile(
+                  movie: _movies[_count],
+                  height: _deviceHeight * 0.20,
+                  width: _deviceWidth * 0.85,
+                ),
+              ),
+            );
+          });
+    } else {
+      return Center(
+        child: CircularProgressIndicator(
+          backgroundColor: Colors.white,
+        ),
+      );
+    }
+  }
+}
